c3d86e4be28e4acd7e15e507ca9f4bc01f90bc77
agarshan <agarshan.senthamipalan@Infor.com>
2022-07-13 01:56:40 +0530

    aga - add age property

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Text, StyleSheet, View, FlatList} from 'react-native'
 
 const ListScreen = () => {
-    const friends = [{name:'friends #1'},{name:'friends #2'},{name:'friends #3'},{name:'friends #4'}]
+    const friends = [{name:'friends #1', age : 20},{name:'friends #2', age : 30},{name:'friends #3', age : 20},{name:'friends #4', age : 20}]
     //const friends = [{name:'friends #1', key: '1'},{name:'friends #2', key: '2'},{name:'friends #3', key: '3'},{name:'friends #4', key: '4'}]
     return <View>
         <FlatList 
@@ -13,7 +13,7 @@ const ListScreen = () => {
         keyExtractor={friend => friends.name} 
         data={friends}  
         renderItem={(item) =>{
-            <Text style={styles.textStyle}>{item.name}</Text>
+            <Text style={styles.textStyle}>{item.name} - Age {item.age}</Text>
         }}></FlatList>
     </View>
 }
