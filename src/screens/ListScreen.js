68d2aaf78329a37b13b1803176cc08ad55994c6c
agarshan <agarshan.senthamipalan@Infor.com>
2022-07-13 01:51:50 +0530

    aga - Flat List props

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -5,10 +5,24 @@ const ListScreen = () => {
     const friends = [{name:'friends #1'},{name:'friends #2'},{name:'friends #3'},{name:'friends #4'}]
     //const friends = [{name:'friends #1', key: '1'},{name:'friends #2', key: '2'},{name:'friends #3', key: '3'},{name:'friends #4', key: '4'}]
     return <View>
-        <FlatList keyExtractor={friend => friends.name} data={friends}  renderItem={(item) =>{
-            <Text>{item.name}</Text>
+        <FlatList 
+        // list will show horizontally
+        horizontal
+        // scroll will be disabled
+        showsHorizontalScrollIndicator = {false}
+        keyExtractor={friend => friends.name} 
+        data={friends}  
+        renderItem={(item) =>{
+            <Text style={styles.textStyle}>{item.name}</Text>
         }}></FlatList>
     </View>
 }
+
+const styles = StyleSheet.create({
+    textStyle : {
+        // this will create extra spacing between text up and down
+        marginVertical : 50
+    }
+})
 
 export default ListScreen;
